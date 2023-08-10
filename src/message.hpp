4455ebe7e212371ed1c11e3bf522338c2ca01a67
Ariel Lira <ariellira.al@gmail.com>
2023-08-10 14:02:14 -0700

    added class for handling messages from websocket

---
+++ src/message.hpp
@@ -0,0 +1,14 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+#include "pch.hpp"
+
+namespace websocket_test {
+struct MessageHandler {
+  explicit MessageHandler(std::function<void(std::span<const char>)> on_message)
+      : OnMessage(on_message) {}
+
+  std::function<void(std::span<const char>)> OnMessage;
+};
+}  // namespace websocket_test
+
+#endif
